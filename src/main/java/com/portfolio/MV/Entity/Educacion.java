9282111a2bcd424f5d40e35604bdc5b5f56e856f
MauricioVibanco <92702962+MauricioVibanco@users.noreply.github.com>
2023-05-13 17:49:55 -0300

    update

--- src/main/java/com/portfolio/MV/Entity/Educacion.java
+++ src/main/java/com/portfolio/MV/Entity/Educacion.java
@@ -5,10 +5,12 @@
  */
 package com.portfolio.MV.Entity;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class Educacion {
