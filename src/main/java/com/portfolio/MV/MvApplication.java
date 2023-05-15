96bffd81d9c7af63ffe46161dc90ae82045d5e84
MauricioVibanco <92702962+MauricioVibanco@users.noreply.github.com>
2023-05-15 01:36:15 -0300

    actualizacion

--- src/main/java/com/portfolio/MV/MvApplication.java
+++ src/main/java/com/portfolio/MV/MvApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.MV;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MvApplication {
@@ -9,5 +12,14 @@ public class MvApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvApplication.class, args);
 	}
-
+@Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("https://frontendmv-46aea.web.app")
+                        .allowedMethods("*").allowedHeaders("*");
+		}
+            };
+        }
 }
