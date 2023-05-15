2565268e0fb3f7b36e041fa99f0aa928a93c8ddf
MauricioVibanco <92702962+MauricioVibanco@users.noreply.github.com>
2023-05-15 13:21:08 -0300

    actalizacion

--- src/main/java/com/portfolio/MV/MvApplication.java
+++ src/main/java/com/portfolio/MV/MvApplication.java
@@ -2,9 +2,7 @@ package com.portfolio.MV;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class MvApplication {
@@ -12,7 +10,8 @@ public class MvApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MvApplication.class, args);
 	}
-@Bean
+}
+/*@Bean
         public WebMvcConfigurer corsConfigurer() {
             return new WebMvcConfigurer() {
             @Override
@@ -23,3 +22,4 @@ public class MvApplication {
             };
         }
 }
+*/
